80a64de6408e4e0691b1d2c0e84af2d89f0cc70d
Julia Tache <jat2167@columbia.edu>
2020-11-15 22:21:21 -0500

    Update pyproject.toml

--- cipher_jat2167/tests/test_cipher_jat2167.py
+++ cipher_jat2167/tests/test_cipher_jat2167.py
@@ -3,3 +3,12 @@ from cipher_jat2167 import cipher_jat2167
 
 def test_version():
     assert __version__ == "0.1.0"
+
+def test_single_word(example):
+    expected = "dnwg"
+    actual = cipher(example, 2)
+    assert actual == expected
+    if actual == expected: 
+        return expected
+
+test_single_word("blue")
