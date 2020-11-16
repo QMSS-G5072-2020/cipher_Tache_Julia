eed899f249e0f584ddb915f1c8c4cae9d2ecb248
Julia Tache <jat2167@columbia.edu>
2020-11-16 11:24:27 -0500

    Update file version

---
+++ cipher_jat2167_2/__init__.py
@@ -0,0 +1,45 @@
+__version__ = "0.2.0"
+
+def cipher(text, shift, encrypt=True):
+"""
+    Encodes a string using a Caesar function which shifts each letter n positions in the alphabet based on a numerical input.
+
+    Parameters
+    ----------
+    text : A string. 
+    shift : An integer. 
+
+    Returns
+    -------
+    An encoded string. 
+
+    Examples
+    --------
+    >>> text = "hello world" 
+    >>> shift = 2
+    >>> alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
+
+new_text: "jgnnq yqtnf"
+
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
